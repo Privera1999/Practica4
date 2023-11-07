2e01b3ab77a7b187a6e57f38ff0dca5bba42d4c8
lopez.lujos <lopez.lujos22@cordoba.salesianos.edu>
2023-11-07 20:57:26 +0100

    subidirt

--- app/src/main/java/com/example/practica4/MainActivity.kt
+++ app/src/main/java/com/example/practica4/MainActivity.kt
@@ -5,7 +5,9 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.animation.animateContentSize
 import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.horizontalScroll
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Arrangement.End
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
@@ -17,6 +19,7 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.AddCircle
@@ -30,13 +33,13 @@ import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import com.example.practica4.R.*
 import com.example.practica4.ui.theme.Practica4Theme
 
 class MainActivity : ComponentActivity() {
@@ -85,24 +88,34 @@ fun BarraBusqueda(
                     imageVector = Icons.Default.AddCircle,
                     contentDescription = null,
                     modifier = Modifier
+                        .heightIn(50.dp)
                         .size(40.dp)
-                        .
+
                 )
+                Spacer(modifier = Modifier.width(6.dp))
                 TextField(
-                    value = "",
+                    value = "¿Qué estás buscando?",
                     onValueChange = {},
-                    leadingIcon = {
-                        Icon(
-                            imageVector = Icons.Default.Search,
-                            contentDescription = null
-                        )
+                    trailingIcon = {
+                        Surface(modifier=Modifier.clip(CircleShape)) {
+                                Icon(
+                                    imageVector = Icons.Default.Search,
+                                    contentDescription = "Introduce",
+                                    modifier = Modifier.size(35.dp)
+                                )
+                        }
                     },
                     modifier = Modifier
                         .heightIn(30.dp)
                 )
             }
         }
-        Icon(imageVector = Icons.Default.Notifications, contentDescription = null)
+        Spacer(modifier = Modifier.width(4.dp))
+        Icon(imageVector = Icons.Default.Notifications,
+            contentDescription = null,
+            modifier = Modifier
+                .heightIn(50.dp)
+                .size(52.dp))
     }
 }
 
